acbeb36287e67885a90222b3ac6edd1bc9636230
Istvan Rozsa <pisti.rzsa@gmail.com>
2021-11-04 11:04:56 +0100

    Add Buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Haligali')
+    }
     return (
         <header>
             <h1>{title}</h1>
+            <Button text = 'Add' onClick={onClick}/>
         </header>
     )
 }
